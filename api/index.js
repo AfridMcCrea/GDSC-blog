672e02601dadab23914a522887d5d11feca0f505
AfridMcCrea <73422624+AfridMcCrea@users.noreply.github.com>
2024-07-28 14:17:28 +0530

    deployment changes

--- api/index.js
+++ api/index.js
@@ -6,13 +6,14 @@ import authRoutes from './routes/auth.routes.js';
 import cookieParser from 'cookie-parser';
 import postRoutes  from './routes/post.routes.js';
 import commentRoutes from './routes/comment.routes.js';
+import path from 'path';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-
+const __dirname = path.resolve();
 
 mongoose.connect(process.env.MONGO).then(() => {
   console.log("mongodb connected");
@@ -26,6 +27,12 @@ app.use('/api/user' , userRoutes);
 app.use('/api/auth' , authRoutes);
 app.use('/api/post' , postRoutes );
 app.use('/api/comment' , commentRoutes);
+
+app.use(express.static(path.join(__dirname , '/client/dist')));
+
+app.get('*' , (req , res) => {
+    res.sendFile(path.join(__dirname, 'client' , 'dist' , 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
